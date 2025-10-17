e180b015e9a91adb2269d271d4a54e5b58db8f63
dirvass <128395952+dirvass@users.noreply.github.com>
2025-10-17 15:05:54 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import BookingPage from "./BookingPage";
 import React, { useEffect, useMemo, useState } from "react";
 import {
   LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, ReferenceLine
