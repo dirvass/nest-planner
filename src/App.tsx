62c06ec64115042e98a32cf986cb9a595931b5c7
dirvass <128395952+dirvass@users.noreply.github.com>
2025-10-17 16:49:52 +0300

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./Home";
+import PlannerPage from "./PlannerPage";
+import BookingPage from "./BookingPage";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/planner" element={<PlannerPage />} />
+        <Route path="/book" element={<BookingPage />} />
+        {/* bilinmeyen path'ler ana sayfaya */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
